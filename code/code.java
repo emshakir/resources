d02260441d6170a223ce977bb89c54f869052106
Shakir <84453380+shakir-fjd@users.noreply.github.com>
2022-08-04 15:40:32 +0530

    Update code.java

--- code/code.java
+++ code/code.java
@@ -2,3 +2,33 @@ RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes(
             if (requestAttributes instanceof ServletRequestAttributes) {
                 request = ((ServletRequestAttributes) requestAttributes).getRequest();
             }
+
+
+
+
+private boolean connecstToFtpServer() throws Exception {
+        boolean ftpLoginStatus = false;
+        try {
+            ftp = new FTPClient();
+            ftp.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
+            ftp.connect(hostname, port);
+            int replyCode = ftp.getReplyCode();
+            if (!FTPReply.isPositiveCompletion(replyCode)) {
+                ftp.disconnect();
+                return ftpLoginStatus;
+            }
+            logger.info("Connected to FTP SERVER, REPLYCODE ::::::: " + replyCode);
+            logger.info("HOST ::::::: " + hostname);
+            logger.info("PORT ::::::: " + port);
+            ftpLoginStatus = ftp.login(username, password);
+            if (ftpLoginStatus) {
+                logger.info("Logged in with username ::::: " + username);
+            } else {
+                logger.info("Unable to Login :::::: ");
+                return ftpLoginStatus;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ftpLoginStatus;
+    }
